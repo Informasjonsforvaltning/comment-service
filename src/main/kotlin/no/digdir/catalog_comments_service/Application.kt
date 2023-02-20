367bc1b2945dc9a66e6528da25c3b357003ef5df
NilsOveTen <github.nils.ove@tendenes.com>
2023-02-20 13:30:45 +0100

    chore: upgrade spring boot

--- src/main/kotlin/no/digdir/catalog_comments_service/Application.kt
+++ src/main/kotlin/no/digdir/catalog_comments_service/Application.kt
@@ -3,9 +3,11 @@ package no.digdir.catalog_comments_service
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableWebSecurity
 open class CommentApplication
 
 fun main(args: Array<String>) {
